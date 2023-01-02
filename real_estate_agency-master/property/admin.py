fc547c661a4e256562f9e066336502b862b7ee55
Vetreria <90621103+Vetreria@users.noreply.github.com>
2023-01-02 07:16:48 +0300

    add search to admin UI.

--- real_estate_agency-master/property/admin.py
+++ real_estate_agency-master/property/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Flat
 
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('owner','town', 'address',)
+ 
+ 
 
-admin.site.register(Flat)
+
+admin.site.register(Flat, AuthorAdmin)
